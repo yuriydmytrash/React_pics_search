26a5c02a2b825e403694b772eacf0f71db30e846
dmytryy <dmytrash.yura@gmail.com>
2019-09-26 21:32:59 -0700

    added unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
+
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: 'Client-ID 48ca5e8e8dbbd8ad2a6f76117ab206f03cc1f9219d3991289318ddf3cf49b619'
+        }
+      });
+    console.log(response.data.results);
   }
   render() {
     return (
